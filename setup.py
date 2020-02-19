0fffc851e8d74a852abd478723820534a4db745d
dust8 <dust8@users.noreply.github.com>
2020-02-19 22:09:04 +0800

    Fixed:找到打包命令

--- setup.py
+++ setup.py
@@ -14,7 +14,6 @@ def read(*parts):
 
 def find_version(*file_paths):
     version_file = read(*file_paths)
-    print(version_file)
     version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]", version_file, re.M)
     if version_match:
         return version_match.group(1)
